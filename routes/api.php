983a7743ef73d2b4d964c5cb13a56540d06e080e
icelake0 <ajiboyeag@gmail.com>
2017-08-04 19:44:16 +0100

    commit

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::get('/getjson', ['middleware' => 'cors', function()
 {
    		$path = storage_path() . "/sim1result.json"; 
 		$json = json_decode(file_get_contents($path), true);
-        return response()->json(['response' => $json]);
+        return response()->json($json);
 }]);
