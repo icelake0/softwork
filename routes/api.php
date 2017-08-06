10bfa508b6669469dbacc928f2f69c11360c3d69
icelake0 <ajiboyeag@gmail.com>
2017-08-06 08:35:21 +0100

    commit for api update

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,70 @@ Route::get('/getjson', ['middleware' => 'cors', function()
 		$json = json_decode(file_get_contents($path), true);
         return response()->json($json);
 }]);
+Route::get('/addjson/{sim1result?}', ['middleware' => 'cors', function(Request $request)
+{
+   		$path = storage_path() . "/sim1result.json"; 
+		File::put($path,$request->sim1result); 
+		//$json = json_decode(file_get_contents($path), true);
+        //return response()->json($json);
+}]);
+Route::get('getsim1Result{matricNumber?}', ['middleware' => 'cors', function(Request $request)
+{		
+	$matricNumber=$request->matricNumber;
+	$path=$matricNumber;
+	if(Storage::exists($path)){
+		$json = json_decode(file_get_contents(storage_path()."/App//".$path."/sim1result.json"), true);
+		return response()->json($json);
+	}
+	else{
+		Storage::makeDirectory($path);
+		//populate the folder with all needed files
+		$sim1result=file_get_contents(storage_path()."/sim1result.json");
+		$sim2result=file_get_contents(storage_path()."/sim2result.json");
+		$sim3result=file_get_contents(storage_path()."/sim3result.json");
+		$sim4result=file_get_contents(storage_path()."/sim4result.json");
+		File::put(storage_path()."/App"."/".$path."/sim1result.json",$sim1result);
+		File::put(storage_path()."/App"."/".$path."/sim2result.json",$sim2result);
+		File::put(storage_path()."/App"."/".$path."/sim3result.json",$sim3result);
+		File::put(storage_path()."/App"."/".$path."/sim4result.json",$sim4result);
+		//$json = json_decode(file_get_contents(storage_path()."/App//".$path."/sim1result.json"), true);
+		//return response()->json($json);
+	}
+}]);
+Route::get('getsim2Result{matricNumber?}', ['middleware' => 'cors', function(Request $request)
+{		
+	$matricNumber=$request->matricNumber;
+	$path=$matricNumber;
+		$json = json_decode(file_get_contents(storage_path()."/App//".$path."/sim2result.json"), true);
+		return response()->json($json);
+}]);
+Route::get('getsim3Result{matricNumber?}', ['middleware' => 'cors', function(Request $request)
+{		
+	$matricNumber=$request->matricNumber;
+	$path=$matricNumber;
+		$json = json_decode(file_get_contents(storage_path()."/App//".$path."/sim3result.json"), true);
+		return response()->json($json);
+}]);
+Route::get('getsim4Result{matricNumber?}', ['middleware' => 'cors', function(Request $request)
+{		
+	$matricNumber=$request->matricNumber;
+	$path=$matricNumber;
+		$json = json_decode(file_get_contents(storage_path()."/App//".$path."/sim4result.json"), true);
+		return response()->json($json);
+}]);
+Route::get('/updateSim1Result/{matricNumber}/{sim1result}', ['middleware' => 'cors', function(Request $request)
+{  		$path = storage_path() ."/app"."/".$request->matricNumber."/sim1result.json";
+		File::put($path,$request->sim1result); 
+}]);
+Route::get('/updateSim2Result/{matricNumber}/{sim2result}', ['middleware' => 'cors', function(Request $request)
+{  		$path = storage_path() ."/app"."/".$request->matricNumber."/sim2result.json";
+		File::put($path,$request->sim2result); 
+}]);
+Route::get('/updateSim3Result/{matricNumber}/{sim3result}', ['middleware' => 'cors', function(Request $request)
+{  		$path = storage_path() ."/app"."/".$request->matricNumber."/sim3result.json";
+		File::put($path,$request->sim3result); 
+}]);
+Route::get('/updateSim4Result/{matricNumber}/{sim4result}', ['middleware' => 'cors', function(Request $request)
+{  		$path = storage_path() ."/app"."/".$request->matricNumber."/sim4result.json";
+		File::put($path,$request->sim4result); 
+}]);
